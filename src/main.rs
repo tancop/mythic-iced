ca5024b8de5fbeb649d61bd024373b1a2652ec31
Teodor Potancok <teodor.potancok@gmail.com>
2026-09-11 20:53:16 +0200

    Add UI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
+use iced::Element;
+use iced::widget::text;
+
 mod colors;
 mod decode;
 mod epic;
 
 fn main() {
     env_logger::init();
-    println!("Hello, world!");
+
+    iced::run(update, view).unwrap();
+}
+
+#[derive(Default)]
+struct State;
+
+enum Message {}
+
+fn update(_: &mut State, _: Message) {}
+
+fn view(_: &State) -> Element<'_, Message> {
+    text!("hi there").into()
 }
