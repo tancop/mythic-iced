423242925ede0bd5305cd5d887f13edcaf287b0a
Teodor Potancok <teodor.potancok@gmail.com>
2026-09-16 11:11:37 +0200

    Update login UI

--- src/ui/login.rs
+++ src/ui/login.rs
@@ -1,26 +1,33 @@
 use iced::{
-    Element,
+    Alignment, Element, Length,
     widget::{button, column, container, text, text_input},
 };
 
 use crate::{Message, State};
 
 pub fn view(_: &State) -> Element<'_, Message> {
-    container(column![
-        text!("Sign in with Epic Games"),
-        button("Open").on_press(Message::StartLogin)
-    ])
-    .center(800)
+    container(
+        column![
+            text!("Sign in with Epic Games"),
+            button("Open").on_press(Message::StartLogin)
+        ]
+        .align_x(Alignment::Center),
+    )
+    .center(Length::Fill)
     .into()
 }
 
 pub fn view_paste_token(state: &State) -> Element<'_, Message> {
-    container(column![
-        text!("Paste your token here:"),
-        text_input("", &state.exchange_code)
-            .on_input(|_| Message::Ignored)
-            .on_paste(|s| Message::SubmitToken(s)),
-    ])
-    .center(800)
+    container(
+        column![
+            text!("Paste your authorizationCode here:"),
+            text_input("56e288...", "")
+                .on_input(|_| Message::Ignored)
+                .on_paste(|s| Message::SubmitToken(s))
+                .width(400),
+        ]
+        .align_x(Alignment::Center),
+    )
+    .center(Length::Fill)
     .into()
 }
